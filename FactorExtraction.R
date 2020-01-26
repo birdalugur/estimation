2458548bd7fd45db57b4b2a4a897c10010508c38
birdalugureren <birdalugureren@gmail.com>
2020-01-27 02:18:25 +0300

    27.01

--- FactorExtraction.R
+++ FactorExtraction.R
@@ -1,5 +1,9 @@
 library(pracma)
 library(Matrix)
+library(RSpectra)
+
+# NOT ----------
+# eigs fonksiyonları kontrol edilmeli
 
 FactorExtraction <- function(x,q,r,p,A,C,Q,R,initX,initV,ss,MM){
   "
@@ -68,14 +72,87 @@ ricSW <- function(x,q,r,p){
   Q[1:r,1:r] <- diag(r)
   OPTS.disp = 0
   
-  "NOT -------------d'nin köşegen matris olması gerekiyor"
-  result_eigs <- eigs(cov(A),k=2,which = "LM")	# computes eigenvalues and eigenvectors of the var-covariance 
-  d <- result_eigs$values
+  "v nin 1. sütununun işareti yanlış -> v[,1]=v[,1]*-1 "
+  result_eigs <- eigs(cov(x),k=r,which = "LM")	# computes eigenvalues and eigenvectors of the var-covariance 
+  d <- diag(length(result_eigs$values))*result_eigs$values
   v <- result_eigs$vectors
+  
+  v[,1]=v[,1]*-1
   
   # matrix of the data, x.
   # d is a rxr diagonal matrix with the 10 largest eigenvalues on the diagonal. 
   # v is a nxr matrix of the eigenvectors that corresponds to the eigenvalues.
+  
+  F <- x%*%v
+  
+  ' cov fonksiyonu aynı girdiye farklı çıktılar verebilir(matlab cov dan farklı) !'
+  R <- diag(diag(cov((x-x%*%v%*%t(v)))))
+  # REMARK: x*v*v' is the projection of x over the principal components (F=x*v)
+
+  if (p>0) { 
+    #ESTIMATE the AUTOregressive model for the Factors: run the var F(t) = A_1*F(t-1)+...+A_p*F(t-1) + e(t);
+    z = F
+    "Matris satırlarının sayısı eşleşmeli ! ! ! !"
+    ############################################# HATALI ##
+    #Z<-matrix(, nrow = size(z)[1], ncol = 0)
+    Z <- c()
+    for (kk in 1:1){
+      Z <- cbind(Z,z[(p-kk+1):(size(z)[1]-kk),]) # stacked regressors (lagged SPC)
+    }
+    ##############################################
+    z<-z[(p+1):size(z)[1],]
+    
+    A_temp <- (inv(t(Z)%*%Z)%*%t(Z))%*%z
+    A[1:r,1:r*p] = t(A_temp) #OLS estimator of the VAR transition matrix
+    
+    # Compute Q
+    e <- z-Z%*%A_temp # VAR residuals
+    H <- cov(e) # VAR covariance matrix
+    
+    if (r==q){
+      # The covariance matrix of the VAR residuals is of full rank
+      Q[1:r,1:r] = H
+    }
+    else{
+      # The covariance matrix of the VAR residuals has reduced rank
+      res_ed <- eigs(H,k=2,which = "LM") # eigenvalue decomposition
+      P <- res_ed$vectors
+      M <- res_ed$values
+      M <- diag(length(M))*M
+      
+      # P<- matrix(c(-0.9530,0.3029,-0.3029,-0.9530), nrow = 2,ncol = 2,byrow = TRUE) 
+      # M<-matrix(c(1.7018,0,0,1.1271),nrow = 2,ncol = 2)
+      
+      P <- e%*%P%*%diag(sign(P[1,]))
+      M ^ (-0.5)
+      
+      "matrix power- (+) tamsayı olmayan k değerinden dolayı hatalı sonuç vermekte"
+      'https://www.rdocumentation.org/packages/expm/versions/0.999-4/topics/matpow'
+      # u_orth = e*P*(M^-.5); # extracting the common shocks
+      
+      
+      e_pc <- e %*% P %*% t(P)
+      Q[1:r,1:r] = P %*% M %*% t(P)
+    }
+  }
+  
+  # Computes the initial conditions for the filter.
+  # The common factors are initialized by the PC estimates.
+  # Initial variance is set equal to the unconditional variance ofthe common factors. 
+  
+  if (p > 0){
+    z <- F
+    Z <- c()
+    for (kk in 1:nlag){
+      Z <- cbind(Z,z[(nlag-kk+1):(size(z)[1]-kk),]) # stacked regressors (lagged SPC)
+      initx <- t(t(Z[1,]))
+    }
+  }
+  else{
+    initx <- c()
+    initv <- c()
+  }
+  
 }
 
 
